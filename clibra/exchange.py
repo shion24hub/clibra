d155fdc953a783df5eb5114ae857bb0d66e35e28
Shion Noguchi <shion24stat@gmail.com>
2024-05-19 00:54:04 +0900

    Formatted code.

--- clibra/exchange.py
+++ clibra/exchange.py
@@ -1,13 +1,13 @@
-from abc import ABCMeta, abstractmethod
 import datetime
 import os
+from abc import ABCMeta, abstractmethod
 from urllib.error import HTTPError
 
 import pandas as pd
 
 
 class Exchange(metaclass=ABCMeta):
-    
+
     @abstractmethod
     def generate_url(self, symbol: str, date: datetime.datetime) -> str:
         pass
@@ -22,24 +22,24 @@ class Exchange(metaclass=ABCMeta):
 
 
 class Bybit(Exchange):
-    
+
     def __init__(self) -> None:
-        self.base_url = 'https://public.bybit.com/trading/'
+        self.base_url = "https://public.bybit.com/trading/"
 
     def generate_url(self, symbol: str, date: datetime.datetime) -> str:
-        """ generate_url
+        """generate_url
 
         add later.
-        
+
         """
 
         filename = f"{symbol}{date.strftime('%Y-%m-%d')}.csv.gz"
         url = os.path.join(self.base_url, symbol, filename)
 
         return url
-    
+
     def download(self, url: str) -> pd.DataFrame | None:
-        """ download
+        """download
 
         add later.
 
@@ -53,12 +53,12 @@ class Bybit(Exchange):
             raise e
 
         return df
-    
+
     def mold(self, df: pd.DataFrame) -> pd.DataFrame:
-        """ mold
+        """mold
 
         add later.
-        
+
         """
 
         df.loc[:, ["datetime"]] = pd.to_datetime(df["timestamp"], unit="s")
--- clibra/main.py
+++ clibra/main.py
@@ -9,21 +9,20 @@ import typer
 from loguru import logger
 from rich import print
 
-from .exchange import (
-    Exchange,
-    Bybit,
-)
-
+from .exchange import Bybit, Exchange
 
 # Constants
 WORKING_DIR = f"{Path.home()}/.clibra"
-IMPLEMENTED_EXCHANGES_MAPPER = {'bybit': Bybit}
+IMPLEMENTED_EXCHANGES_MAPPER = {"bybit": Bybit}
 
 logger.remove()
-logger.add(f"{WORKING_DIR}/log/app.log", level="INFO", format="{time} {level} {message}")
+logger.add(
+    f"{WORKING_DIR}/log/app.log", level="INFO", format="{time} {level} {message}"
+)
 app = typer.Typer()
 
 # Utility Functions
+
 
 def make_1s_candle(df: pd.DataFrame):
     """
@@ -68,6 +67,7 @@ def make_1s_candle(df: pd.DataFrame):
 
     return df
 
+
 def generate_save_path(exchange: str, symbol: str, date: datetime.datetime) -> str:
     """
     Define the storage path.
@@ -75,13 +75,14 @@ def generate_save_path(exchange: str, symbol: str, date: datetime.datetime) -> s
 
     save_path = os.path.join(
         WORKING_DIR,
-        'candles',
+        "candles",
         exchange,
         symbol,
     )
     filename = f'{date.strftime("%Y-%m-%d")}.csv.gz'
 
     return save_path, filename
+
 
 def validate_datetime_format(date: str) -> datetime.datetime:
     """
@@ -95,6 +96,7 @@ def validate_datetime_format(date: str) -> datetime.datetime:
         raise typer.BadParameter(err)
 
     return dt
+
 
 def validate_exchange(exchange: str) -> str:
     """
@@ -111,9 +113,10 @@ def validate_exchange(exchange: str) -> str:
 
 # CLI Commands
 
+
 @app.callback(help="A CLI tool for managing the crypto candlestick data.")
 def callback() -> None:
-    """ callback
+    """callback
     initialize the working directory.
     """
 
@@ -127,7 +130,7 @@ def update(
     begin: str = typer.Argument(help="The begin date (YYYYMMDD)"),
     end: str = typer.Argument(help="The end date (YYYYMMDD)"),
 ) -> None:
-    """ update
+    """update
 
     update data directory of 1-second candlestick data.
 
@@ -137,11 +140,11 @@ def update(
 
     # validate the exchange
     l_exchange = validate_exchange(exchange)
-    
+
     # validate the date format
     bdt = validate_datetime_format(begin)
     edt = validate_datetime_format(end)
-    
+
     # main process
     # 1. generate the date range
     # 2. check if the data already exists
@@ -156,11 +159,11 @@ def update(
 
         target_dir, target_file = generate_save_path(l_exchange, symbol, date)
         target = os.path.join(target_dir, target_file)
-        print(f'Target: {target}')
+        print(f"Target: {target}")
 
         # check if the data already exists
         if os.path.exists(target):
-            print(f'    - Already exists.')
+            print(f"    - Already exists.")
             continue
 
         # download the data
@@ -174,7 +177,7 @@ def update(
             print(f"    - An error occurred: {e}.")
             continue
         print(f"    - Downloaded {url}.")
-        
+
         # data processing
         df = ex.mold(df)
         df = make_1s_candle(df)
@@ -184,9 +187,9 @@ def update(
         os.makedirs(target_dir, exist_ok=True)
         df.to_csv(target, compression="gzip")
         print(f"    - Saved the data.")
-    
-    print('All processes are completed.')
-    print(f'Elapsed time: {datetime.datetime.now() - timer}')
+
+    print("All processes are completed.")
+    print(f"Elapsed time: {datetime.datetime.now() - timer}")
 
 
 @app.command(help="Generate the csv.gz file of T seconds OHLCV.")
@@ -198,7 +201,7 @@ def generate(
     interval: int = typer.Argument(help="The interval in seconds"),
     output_dir: str = typer.Option("./", help="The output directory"),
 ) -> None:
-    """ generate
+    """generate
 
     generate the database of 1-second candlestick data.
 
@@ -221,19 +224,21 @@ def generate(
     # 5. save the data
 
     date_range = pd.date_range(bdt, edt, freq="D")
-    dfs: list[pd.DataFrame] = [] # https://github.com/microsoft/pylance-release/issues/5630
+    dfs: list[pd.DataFrame] = (
+        []
+    )  # https://github.com/microsoft/pylance-release/issues/5630
 
     for date in date_range:
-        
+
         target_dir, target_file = generate_save_path(l_exchange, symbol, date)
         target = os.path.join(target_dir, target_file)
-        print(f'Target: {target}')
+        print(f"Target: {target}")
 
         # check if the data exists
         if not os.path.exists(target):
-            print('    - does not exist.')
+            print("    - does not exist.")
             continue
-        
+
         # read the data and convert its type
         df = pd.read_csv(target)
         df["datetime"] = pd.to_datetime(df["datetime"])
@@ -265,43 +270,45 @@ def generate(
 
     # save
     if len(dfs) == 0:
-        print('No data.')
+        print("No data.")
         return
-    
+
     ans = pd.concat(dfs)
     file_name = f"{exchange}_{symbol}_{begin}_{end}_{interval}.csv.gz"
     output_path = os.path.join(output_dir, file_name)
     ans.to_csv(output_path, compression="gzip")
 
-    print(f'Saved the data to {output_path}.')
-    print(f'All processes are completed.')
-    print(f'Elapsed time: {datetime.datetime.now() - timer}')
+    print(f"Saved the data to {output_path}.")
+    print(f"All processes are completed.")
+    print(f"Elapsed time: {datetime.datetime.now() - timer}")
 
 
-@app.command(help='Remove data for a specified period.')
+@app.command(help="Remove data for a specified period.")
 def remove(
     exchange: str = typer.Argument(help="The exchange to remove"),
     symbol: str = typer.Argument(help="The symbol to remove"),
     begin: str = typer.Argument(help="The begin date (YYYYMMDD)"),
     end: str = typer.Argument(help="The end date (YYYYMMDD)"),
 ) -> None:
-    """ remove
+    """remove
 
     Delete data stored
 
     """
 
     timer = datetime.datetime.now()
-    
+
     # validate the exchange
     l_exchange = validate_exchange(exchange)
-    
+
     # validate the date format
     bdt = validate_datetime_format(begin)
     edt = validate_datetime_format(end)
 
     # confirm
-    y_or_n = typer.prompt(f"Do you really want to remove {bdt}-{edt} data for {symbol}? (y/n)")
+    y_or_n = typer.prompt(
+        f"Do you really want to remove {bdt}-{edt} data for {symbol}? (y/n)"
+    )
     if y_or_n != "y":
         print("Canceled.")
         return
@@ -311,26 +318,26 @@ def remove(
     # 2. remove the data
 
     date_range = pd.date_range(bdt, edt, freq="D")
-    
+
     for date in date_range:
-        
+
         target_dir, target_file = generate_save_path(l_exchange, symbol, date)
         target = os.path.join(target_dir, target_file)
-        print(f'Target: {target}')
+        print(f"Target: {target}")
 
         if os.path.exists(target):
             os.remove(target)
             print(f"    - removed.")
         else:
             print(f"    - does not exist.")
-    
-    print('All processes are completed.')
-    print(f'Elapsed time: {datetime.datetime.now() - timer}')
+
+    print("All processes are completed.")
+    print(f"Elapsed time: {datetime.datetime.now() - timer}")
 
 
-@app.command(help='Show the available symbols and dates.')
+@app.command(help="Show the available symbols and dates.")
 def show() -> None:
-    """ show
+    """show
 
     show the available symbols and dates.
 
@@ -341,7 +348,7 @@ def show() -> None:
     for root, _, files in os.walk(WORKING_DIR):
         for file in files:
             total_size += os.path.getsize(os.path.join(root, file))
-    
+
     print(f"Total size: {total_size / 1024 / 1024:.2f} MB")
 
     # main process
@@ -349,22 +356,27 @@ def show() -> None:
     exchanges = [x for x in exchanges if not x.startswith(".")]
 
     for exchange in exchanges:
-            
+
         symbols = os.listdir(f"{WORKING_DIR}/candles/{exchange}")
         symbols = [x for x in symbols if not x.startswith(".")]
 
         for symbol in symbols:
-            
+
             dates = os.listdir(f"{WORKING_DIR}/candles/{exchange}/{symbol}")
             dates = [x.split(".")[0] for x in dates]
             dates = [datetime.datetime.strptime(x, "%Y-%m-%d") for x in dates]
 
             mindt = min(dates)
             maxdt = max(dates)
-            missings = [x for x in pd.date_range(mindt, maxdt, freq="D") if x not in dates]
+            missings = [
+                x for x in pd.date_range(mindt, maxdt, freq="D") if x not in dates
+            ]
 
             message = "{}: {} from {} to {}, {} missing dates".format(
-                exchange, symbol, mindt.strftime("%Y-%m-%d"), maxdt.strftime("%Y-%m-%d"), len(missings)
+                exchange,
+                symbol,
+                mindt.strftime("%Y-%m-%d"),
+                maxdt.strftime("%Y-%m-%d"),
+                len(missings),
             )
             print(message)
-
